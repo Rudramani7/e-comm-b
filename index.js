37fe80796feb191c777f652086a70d90de6f170f
Rudramani7 <rudramani1410@gmail.com>
2022-03-31 21:07:16 +0530

    git

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-const PORT = 4000;
 
 const mobiles = [
     {
@@ -28,14 +31,44 @@ const mobiles = [
       }
 ];
 
+const PORT = process.env.PORT; 
+
+const MONGO_URL =  process.env.MONGO_URL;
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongdb is Connected ✌😊");
+  return client;
+}
+
+const client = await createConnection();
+
+
 app.use(cors());
+app.use(express.json());
 
 app.get("/", function (req, res){
     res.send("Hello 🌍!!!");
 });
 
-app.get("/mobiles", function (req, res){
-    res.send(mobiles);
+app.get("/mobiles",async function (req, res){
+  const mobiles = await client
+  .db("shop")
+  .collection("mobiles")
+  .find({})
+  .toArray();
+  res.send(mobiles);
+});
+
+app.post("/mobiles", async function (req, res){
+  const data = req.body;
+  console.log(data);
+  const result = await client
+  .db("shop")
+  .collection("mobiles")
+  .insertMany(data);
+  res.send(result);
 });
 
 app.listen(PORT, ()=> console.log("App started in",PORT));
